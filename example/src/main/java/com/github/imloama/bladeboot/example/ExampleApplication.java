18b4281eddc57ab60f7bd7f7ceed885d62666378
max <imloama@gmail.com>
2019-03-04 21:53:41 +0800

    example

---
+++ example/src/main/java/com/github/imloama/bladeboot/example/ExampleApplication.java
@@ -0,0 +1,11 @@
+package com.github.imloama.bladeboot.example;
+
+import com.github.imloama.bladeboot.BladeApplication;
+
+public class ExampleApplication {
+
+    public static void main(String[] args) {
+        BladeApplication.run(ExampleApplication.class, args);
+    }
+
+}
